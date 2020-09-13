0ec0aad3e7141bac06d4e44e56b89562de03172d
Osir Gueye <osir.gueye@numberly.com>
2020-09-13 15:13:43 +0200

    Add message cell design

--- src/components/messages/messageCell.vue
+++ src/components/messages/messageCell.vue
@@ -1,24 +1,33 @@
 <template>
   <div
     class="message"
-    :class="{read: message.read}"
-    @click="$emit('click', message.id)"
+    :class="{bold: !message.read}"
+    @click="$emit('click')"
   >
     <el-row>
-      <el-col :span="20">
-        <i :class="type[message.type].icons" />
-        <div class="contactName">
+      <el-col :span="19">
+        <i
+          class="icon"
+          :class="type[message.type].icons"
+        />
+        <span class="contactName">
           {{ message.contact.firstname }} {{ message.contact.lastname }}
-        </div>
+        </span>
+        <span
+          v-if="message.type === 'phone'"
+          class="phone"
+        >({{ message.contact.phone }})</span>
       </el-col>
-      <el-col :span="4">
-        <span>{{ message.date }}</span>
+      <el-col :span="5">
+        <span class="date">{{ getDate(message.date) }}</span>
       </el-col>
     </el-row>
-    <div>{{ type[message.type].text }}</div>
+    <div class="messageType">
+      {{ type[message.type].text }}
+    </div>
     <!-- missing phone duration on api return -->
     <div
-      v-if="message.type ==='email'"
+      v-if="message.type ==='email' || message.type === 'sms'"
       class="body"
     >
       {{ message.body }}
@@ -27,6 +36,8 @@
 </template>
 
 <script>
+import dateFormat from '@/helpers/dateFormatter';
+
 export default {
   name: 'MessageCell',
   props: {
@@ -40,24 +51,72 @@ export default {
     return ({
       type: {
         phone: { icons: 'el-icon-phone', text: 'Appel téléphonique depuis MeilleursAgents' },
-        email: { icons: 'el-icon-message', text: 'Message sur votre vitrine MeilleursAgents' }
+        email: { icons: 'el-icon-message', text: 'Message sur votre vitrine MeilleursAgents' },
+        sms: { icons: 'el-icon-chat-dot-round', text: 'SMS depuis MeilleursAgents' },
       }
     });
+  },
+  methods: {
+    getDate(date) {
+      return dateFormat(date);
+    }
   }
 };
 </script>
 <style lang="scss">
-  .message {
-    background-color: white;
-    margin-bottom: 10px;
-    cursor: pointer;
-    padding: 10px;
-  }
-  .body {
-    height: 20px;
-    overflow:hidden
+@import '@/scss/main.scss';
+
+
+.message {
+  background-color: white;
+  cursor: pointer;
+  padding: 10px;
+  border-bottom: 1px solid $background-color;
+}
+.body {
+  height: 20px;
+  overflow:hidden;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 12px;
+  color: $light-text;
+  margin-top: 5px;
+  margin-left: 20px;
+}
+.contactName {
+  display: inline;
+  font-size: 18px;
+}
+.phone {
+  font-size: 16px;
+}
+.messageType {
+  margin-top: 5px;
+  font-size: 14px;
+  color: $light-text;
+  margin-left: 20px;
+}
+.date {
+  font-size: 12px;
+  float: right;
+}
+.icon {
+  color: $light-text
+}
+.bold {
+  .date {
+    color: $text-blue;
   }
   .contactName {
-    display: inline;
+    color: black;
+    font-weight: 500;
   }
+  .messageType {
+    color: black;
+  }
+  .icon {
+    color: $text-blue
+  }
+}
 </style>
