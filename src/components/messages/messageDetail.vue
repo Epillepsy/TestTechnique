d416764492568932fef8ec84527d7daaf762d7d1
Osir Gueye <osir.gueye@numberly.com>
2020-09-13 15:13:56 +0200

    Add message details design

--- src/components/messages/messageDetail.vue
+++ src/components/messages/messageDetail.vue
@@ -1,23 +1,66 @@
 <template>
   <div class="detail">
-    <div class="messageContact">
-      {{ message.contact.firstname }} {{ message.contact.lastname }}
-      {{ message.contact.phone }}
-      {{ message.contact.email }}
-    </div>
-    <div class="messageBody">
-      <h2>
-        {{ message.contact.firstname }} {{ message.contact.lastname }}
-      </h2>
-      <span>{{ message.date }}</span>
-      <div>
-        {{ message.body }}
-      </div>
-    </div>
+    <el-row>
+      <el-col
+        :span="23"
+        class="section bold contactHeader"
+      >
+        <div class="contactName">
+          {{ message.contact.firstname }} {{ message.contact.lastname }}
+        </div>
+        <el-row>
+          <el-col :span="12">
+            Email
+          </el-col>
+          <el-col
+            :span="12"
+            class="text-blue"
+          >
+            {{ message.contact.email }}
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            Telephone
+          </el-col>
+          <el-col
+            :span="12"
+            class="text-blue"
+          >
+            {{ message.contact.phone }}
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col
+        :span="23"
+        class="section messageBody"
+      >
+        <div
+          class="bold contactName"
+        >
+          {{ message.contact.firstname }} {{ message.contact.lastname }}
+        </div>
+        <!-- Better to avoid inline css but too specific to factorize in class -->
+        <div
+          style="margin-top: 12px;"
+          class="text-light"
+        >
+          {{ formatDate(message.date) }}
+        </div>
+        <div
+          style="margin-top: 35px;"
+        >
+          {{ message.body }}
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
+import Moment from 'moment';
+
 export default {
   name: 'MessageDetails',
   props: {
@@ -26,6 +69,33 @@ export default {
       default: () => {},
       required: true
     }
+  },
+  methods: {
+    formatDate(date) {
+      // Prefer using built in locale format than a manual concatenation
+      return new Moment(date).format('LLL');
+    }
   }
 };
 </script>
+
+<style lang="scss">
+.section {
+  padding: 20px;
+  background-color: white;
+  margin-bottom: 20px;
+}
+.messageBody {
+
+}
+.contactHeader {
+  padding: 20px;
+  margin-top: 20px;
+  background-color: white;
+  & > {
+    .el-row {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
